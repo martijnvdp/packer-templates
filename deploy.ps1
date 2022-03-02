10daf0dce7313e4c60215a95421975b8d4c783ac
martijnvdp <m.vanderploeg@bl.ccv.eu>
2022-03-02 09:23:09 +0100

    fmt

--- deploy.ps1
+++ deploy.ps1
@@ -9,10 +9,12 @@ if (!(test-path $buildVarFile)) { $buildVarFile = "variables/build/vsphere-iso.p
 if (!$credential) { $credential = get-credential }
 $templates = (Get-ChildItem .\variables\os\).name
 $menu = @{}
+
 for ($i = 1; $i -le $templates.count; $i++) {
     Write-Host "$i. $($templates[$i-1])" 
     $menu.Add($i, ($templates[$i - 1], $($templates[$i - 1].split(".")[0] + "." + $templates[$i - 1].split(".")[1])))
 }
+
 if (!$all.IsPresent) {
     [int]$option = Read-Host 'Enter selection (enter 0 to deploy all templates)'
 }
@@ -24,9 +26,7 @@ if ($option -eq 0 -or $all.IsPresent) {
         packer build -force --only=$($menu.item($option)[1]) --var-file=$buildVarFile  --var-file="variables/os/$($menu.item($option)[0])" -var "vcenter_username=$($Credential.username)"  -var "vcenter_password=$($Credential.GetNetworkCredential().Password)" .
     }
 }
-
 else {
     packer build -force --only=$($menu.item($option)[1]) --var-file=$buildVarFile --var-file="variables/os/$($menu.item($option)[0])" -var "vcenter_username=$($Credential.username)"  -var "vcenter_password=$($Credential.GetNetworkCredential().Password)" .    
 }
-
 
